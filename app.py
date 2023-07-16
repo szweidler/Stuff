8a88d0c2084a225bcb9a3e5de76d1dafce8ad0a7
szweidler <139645311+szweidler@users.noreply.github.com>
2023-07-16 19:42:50 +0000

    adapt of code

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 import streamlit as st
 
 def main():
+    title = "Herzlichen Dank!"
+    subtitle = "Moon&Stars23"
+    emoji = "&#10024;"
+    title_color = "#000000"  # Black color
+    subtitle_color = "#9c72a9"  # Hex color code for #9c72a9
+
     st.title("Streamlit Website with Title and Subtext")
 
-    # Get user input for title, subtext, color, and emoji
-    title = st.text_input("Enter the title:")
-    subtext = st.text_area("Enter the subtext:")
-    color = st.color_picker("Choose a color for the title", "#000000")
-    emoji = st.text_input("Insert an emoji for the title (e.g., :smile:):")
+    st.markdown(f"<h1 style='color:{title_color};'>{emoji} {title}</h1>", unsafe_allow_html=True)
 
-    # Display the title with the selected color and emoji (if provided)
-    title_with_emoji = f"{emoji} {title}" if emoji else title
-    st.markdown(f"<h1 style='color:{color};'>{title_with_emoji}</h1>", unsafe_allow_html=True)
-
-    # Display the subtext
-    st.subheader("Subtext:")
-    st.write(subtext)
+    st.subheader("Subtitle:")
+    st.markdown(f"<h2 style='color:{subtitle_color};'>{subtitle}</h2>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
