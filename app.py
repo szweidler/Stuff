28e2a580818dd8843944b2c8914680634ea6c01f
szweidler <139645311+szweidler@users.noreply.github.com>
2023-07-16 21:05:10 +0000

    insert photocode

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+import requests
+from PIL import Image
+from io import BytesIO
+
 def main():
     title = "Herzlichen Dank!"
     subtitle = "Moon&Stars23"
@@ -14,6 +18,22 @@ def main():
 
     # st.subheader("Subtitle:")
     st.markdown(f"<h2 style='color:{subtitle_color}; text-align:center;'>{subtitle}</h2>", unsafe_allow_html=True)
+    
+    # Fetch the image from GitHub
+    image_url = "https://raw.githubusercontent.com/szweidler/Stuff/main/photos/TitlePic.jpg"
+    image = fetch_image(image_url)
 
+    if image is not None:
+        st.image(image, use_column_width=True)
+
+
+def fetch_image(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return Image.open(BytesIO(response.content))
+    except Exception as e:
+        st.error(f"Error fetching the image: {e}")
+    return None
 if __name__ == "__main__":
     main()
