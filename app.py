12a556a1247fc06ace666da4af04d8c612788578
szweidler <139645311+szweidler@users.noreply.github.com>
2023-07-16 19:46:19 +0000

    change in code

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ def main():
     title_color = "#000000"  # Black color
     subtitle_color = "#9c72a9"  # Hex color code for #9c72a9
 
-    st.title("Streamlit Website with Title and Subtext")
+    # st.title("")
 
     st.markdown(f"<h1 style='color:{title_color};'>{emoji} {title}</h1>", unsafe_allow_html=True)
 
-    st.subheader("Subtitle:")
+    # st.subheader("Subtitle:")
     st.markdown(f"<h2 style='color:{subtitle_color};'>{subtitle}</h2>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
